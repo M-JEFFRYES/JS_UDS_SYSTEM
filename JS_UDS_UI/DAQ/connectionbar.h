275563231b3d1e88d775b7f2a769ae8db4895012
mjeffryes <mike.jeffryes@hotmail.com>
2022-07-27 16:56:26 +0100

    Conn bar added

---
+++ JS_UDS_UI/DAQ/connectionbar.h
@@ -0,0 +1,42 @@
+#ifndef CONNECTIONBAR_H
+#define CONNECTIONBAR_H
+
+#include <QFrame>
+#include <QPushButton>
+
+#include <QtSerialPort/QSerialPort>
+#include <QtSerialPort/QSerialPortInfo>
+
+namespace Ui {
+class ConnectionBar;
+}
+
+class ConnectionBar : public QFrame
+{
+    Q_OBJECT
+
+public:
+    explicit ConnectionBar(QWidget *parent = nullptr);
+    ~ConnectionBar();
+
+    void setBarHeights(int minimum, int maximum);
+    void setEnterRecordingMode(bool start);
+
+    bool isPortOpen();
+    QPushButton* getConnectionButton();
+    QPushButton* getRefreshButton();
+    QString getSelectedPort();
+
+private slots:
+    void on_connectButton_clicked();
+    void on_refreshButton_clicked();
+
+private:
+    Ui::ConnectionBar *ui;
+    void initButtons();
+    void setConnected();
+    void setDisconnected();
+    void getOpenPorts();
+};
+
+#endif // CONNECTIONBAR_H
