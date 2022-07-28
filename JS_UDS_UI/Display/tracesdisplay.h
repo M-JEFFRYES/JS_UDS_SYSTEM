5b22330ca34db98f7170af1079e018e03d059889
mjeffryes <mike.jeffryes@hotmail.com>
2022-07-28 11:21:07 +0100

    Graph class added

--- JS_UDS_UI/Display/tracesdisplay.h
+++ JS_UDS_UI/Display/tracesdisplay.h
@@ -15,8 +15,17 @@ public:
     explicit TracesDisplay(QWidget *parent = nullptr);
     ~TracesDisplay();
 
+    void loadCurrentDataset(std::map<QString, double> dataset);
+
 private:
     Ui::TracesDisplay *ui;
+
+    void initGraph();
+    void initDataset(QVector<QString> channel_names,int no_samples);
+
+    int no_samples;
+    QVector<QString> channel_names;
+    QVector<QVector<double>> dataset;
 };
 
 #endif // TRACESDISPLAY_H
